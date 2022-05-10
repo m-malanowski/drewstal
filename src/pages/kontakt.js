ef18afbcc26ba018116a83378ca838b403f14303
Mateusz Malanowski <38919740+m-malanowski@users.noreply.github.com>
2022-05-10 11:30:10 +0200

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -48,7 +48,7 @@ const contactPage = () => (
                                 transition: { ...transition},
                             }}
                 >
-                    <p>TEL: +48 663 45 45 45</p>
+                    <p>TEL: +48 603 723 713</p>
                     <p>EMAIL: <a href="mailto:biuro@drewstal.com.pl">biuro@drewstal.com.pl</a></p>
                     <p>GODZINY OTWARCIA: <br/> Pn-pt: 8:00 - 16:00, <br/> Sobota: Zamknięte </p>
                     <p className="mt-5 find"><a href="https://g.page/drewstal?gm">Tu nas znajdziesz</a></p>
